717df0110f82dd47c571505f3f9396607cf70e38
s504 <s504@salas.local>
2019-08-29 20:47:12 -0500

    first commit

---
+++ src/clienteservidor/MainCliente.java
@@ -0,0 +1,17 @@
+
+package clienteservidor;
+
+import java.io.IOException;
+
+
+public class MainCliente {
+    
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+    
+}
---
+++ src/clienteservidor/MainServidor.java
@@ -0,0 +1,16 @@
+package clienteservidor;
+
+
+
+import java.io.IOException;
+
+public class MainServidor {
+
+    public static void main(String[] args) throws IOException
+    {
+        Servidor serv = new Servidor(); //Se crea el servidor
+
+        System.out.println("Iniciando servidor\n");
+        serv.startServer(); //Se inicia el servidor
+    }
+}
